4cebad5f6eedeccfd0c1c1d9564cd06801b0ef22
kavyasri-1625 <kavyasreemajeti@gmail.com>
2025-11-03 22:32:57 +0530

    Configured backend API URL and built production files

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  baseURL: 'https://expense-tracker-server-k6d0.onrender.com',
 })
 
 instance.interceptors.request.use((config) => {
